aff43ac0dd1e3d33b444c206fd0facb7743deb80
HillTopTRPG <mihikari90@gmail.com>
2019-10-19 01:25:43 +0900

    Ver.1.0.0a3 :sparkles: 公式サーバを借りられたので、それに向けて整備

---
+++ src/nekostore_test/test_server.ts
@@ -0,0 +1,26 @@
+import BasicDriver from "nekostore/lib/driver/basic";
+import SocketDriverServer from "nekostore/lib/driver/socket/SocketDriverServer";
+
+async function main(): Promise<void> {
+  try {
+    const driver = new BasicDriver();
+    const io = require('socket.io').listen(8000);
+
+    console.log("Quoridorn Server is Ready.");
+
+    io.on("connection", (socket: any) => {
+      console.log("Connected", socket.id);
+
+      socket.on('disconnect', () => {
+        console.log('disconnected', socket.id);
+      });
+
+      // nekostore起動！
+      new SocketDriverServer(driver, socket);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
